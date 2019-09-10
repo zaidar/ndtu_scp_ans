0fa3acd0d7ae7b4a08fcc38e439612d36be81418
zssv aid <30953478+zaidar@users.noreply.github.com>
2019-09-11 02:50:05 +0600

    updte bot robotay

--- 2/35.rkt
+++ 2/35.rkt
@@ -1,6 +1,26 @@
 #lang racket ;2.73
-(require racket/include)
-(include "defines.rkt")
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (square x)(* x x))
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (addend s) (cadr s))
+(define (augend s) (caddr s))
+
+(define (make-sum a1 a2) (list '+ a1 a2))
+(define (make-product m1 m2) (list '* m1 m2))
+
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
 
 (define (deriv exp var) 
   (cond ((number? exp) 0)
